a1cb7be2e13b6ed58d387ba2502b22d242d8ac7c
Aakamsh PM <aakamshpm@gmail.com>
2023-09-03 23:54:24 +0530

    add product form included

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var hbs = require("express-handlebars");
+var fileUpload = require("express-fileupload");
 
 var userRouter = require("./routes/user");
 var adminRouter = require("./routes/admin");
@@ -25,9 +26,10 @@ app.engine(
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(fileUpload());
 
 app.use("/", userRouter);
 app.use("/admin", adminRouter);
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-/* GET users listing. */
+
 router.get("/", function (req, res, next) {
   const products = [
     {
@@ -33,6 +33,15 @@ router.get("/", function (req, res, next) {
     },
   ];
   res.render("admin/view-products", { products, admin: true });
+});
+
+router.get("/add-product", (req, res) => {
+  res.render("admin/add-product");
+});
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  console.log(req.files.image);
 });
 
 module.exports = router;
